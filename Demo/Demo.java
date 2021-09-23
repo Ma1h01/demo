0102599c8d9652fa4b50b6bf44f46212e12a029d
Yihao Mai <ym93309@csci-odin.cs.uga.edu>
2021-09-23 00:04:03 -0400

    First Commit

--- Demo/Demo.java
+++
@@ -1,7 +0,0 @@
-package demo.Demo;
-
-public class Demo {
-    public static void main(String[] args){
-        System.out.println("#1");
-    }
-}
---
+++ src/Demo/Task.java
@@ -0,0 +1,9 @@
+package Demo;
+
+public class Task {
+    public static void main (String[] args) {
+        for (int i = 0; i < 101; i++) {
+            System.out.println("#"+i);
+        }
+    }
+}
